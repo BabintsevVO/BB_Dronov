8d6836635838d0d41d32b29661a86c3e8482f47d
BabintsevVO <babintsevvo@gmail.com>
2023-03-02 08:24:25 +0300

    add profile and logout

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView
 
 app_name = 'main'
 urlpatterns = [
     path('<str:page>/', other_page, name='other'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
+    path('accounts/profile/', profile, name='profile'),
+    path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
     path('', index, name='index'),
 ]
+
