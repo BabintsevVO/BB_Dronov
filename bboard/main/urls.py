f07308efe22561ba042b4e711873bb3cba475178
BabintsevVO <babintsevvo@gmail.com>
2023-03-02 19:25:42 +0300

    add register (code not completed)

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView
+from .views import index, other_page, BBLoginView, profile, BBLogoutView, ChangeUserInfoView, BBPasswordChangeView, RegisterUserView, RegisterDoneView
 
 app_name = 'main'
+
 urlpatterns = [
     path('<str:page>/', other_page, name='other'),
+    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
+    path('accounts/register/', RegisterUserView.as_view(), name='register'),
     path('accounts/login/', BBLoginView.as_view(), name='login'),
     path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
     path('accounts/profile/', profile, name='profile'),
