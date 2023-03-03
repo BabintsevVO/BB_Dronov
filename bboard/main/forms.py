824caf8ca73521cbf9597badca8aaa08aeba9c3b
BabintsevVO <babintsevvo@gmail.com>
2023-03-03 18:44:21 +0300

    add view-delete user,  and admin

--- bboard/main/forms.py
+++ bboard/main/forms.py
@@ -31,23 +31,23 @@ class RegisterUserForm(forms.ModelForm):
         password2 = self.cleaned_data['password2']
         if password1 and password2 and password1 != password2:
             errors = {
-                'password2': ValidationError('Введенные пароли не совпадают', code = 'password_mismatch')
+                'password2': ValidationError('Введенные пароли не совпадают', code='password_mismatch')
             }
             raise ValidationError(errors)
 
-        def save(self, commit=True):
-            user = super().save(commit=False)
-            user.set_password(self.cleaned_data['password1'])
-            user.is_active = False
-            user.is_activated = False
-            if commit:
-                user.save()
-            user_registered.send(RegisterUserForm, instance=user)
-            return user
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        user.is_active = False
+        user.is_activated = False
+        if commit:
+            user.save()
+        user_registered.send(RegisterUserForm, instance=user)
+        return user
 
-        class Meta:
-            model = AdvUser
-            fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'send_messages')
+    class Meta:
+        model = AdvUser
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'send_messages')
 
 
 
--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import TemplateDoesNotExist
 from django.template.loader import get_template
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic.edit import UpdateView, CreateView
+from django.contrib.auth import logout
 from django.contrib.messages.views import SuccessMessageMixin
-from django.urls import reverse_lazy
-from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.views.generic.edit import UpdateView, CreateView, DeleteView
 from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
+from django.core.signing import BadSignature
+
 
 from .models import AdvUser
 from .forms import ChangeUserInfoForm, RegisterUserForm
+from .utilites import signer
 
 
 def index(request):
@@ -72,4 +76,41 @@ class RegisterUserView(CreateView):
 
 class RegisterDoneView(TemplateView):
     template_name = 'main/register_done.html'
+
+
+def user_activate(request, sign):
+    try:
+        username = signer.unsign(sign)
+    except BadSignature:
+        return render(request, 'main/bad_signature.html')
+    user = get_object_or_404(AdvUser, username=username)
+    if user.is_activated:
+        template = 'main/user_is_activated.html'
+    else:
+        template = 'main/activation_done.html'
+        user.is_active = True
+        user.is_activated = True
+        user.save()
+    return render(request, template)
+
+
+class DeleteUserView(LoginRequiredMixin, DeleteView):
+    model = AdvUser
+    template_name = 'main/delete_user.html'
+    success_url = reverse_lazy('main:index')
+
+    def setup(self, request, *args, **kwargs):
+        self.user_id = request.user.pk
+        return super().setup(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        logout(request)
+        messages.add_message(request, messages.SUCCESS, 'пользователь удален')
+        return super().post(request, *args, **kwargs)
+
+    def get_object(self, queryset=None):
+        if not queryset:
+            queryset = self.get_queryset()
+        return get_object_or_404(queryset, pk=self.user_id)
+
 
